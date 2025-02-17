2c7134ec2b4b2b87c4daaa95746ad13399318ab0
suspencer14 <spencer.delange@gmail.com>
2025-02-16 17:03:19 -0700

    Specify parking reservation site

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-### BrightonParkingReserver9000
+### Utah Parking Notifier
 
 #### Install / Usage
 - pip install -r requirements.txt
 - playwright install
-- python reserver9000.py 'Sunday, January 19, 2025'
+- python reserver9000.py "Sunday, January 19, 2025" https://reserve.altaparking.com/select-parking
--- reserver9000.py
+++ reserver9000.py
@@ -4,13 +4,18 @@ from playwright.sync_api import sync_playwright
 from win10toast_click import ToastNotifier
        
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python reserver9000.py <date>")
-        print("e.g. 'python reserver9000.py Friday, February 21, 2025'")
+    if len(sys.argv) < 3:
+        print("Usage: python reserver9000.py <date> <parking reservation site>")
+        print("e.g. 'python reserver9000.py \"Friday, February 21, 2025\" https://reserve.altaparking.com/select-parking'")
+        print("Note: Put the date in quotes if it contains spaces")
         sys.exit(1)
-
-    url = "https://reservenski.parkbrightonresort.com/select-parking"
+    
     date = sys.argv[1]
+    url = sys.argv[2]
+    
+    print(f"Checking for availability on {date}")
+    print(f"URL: {url}")
+    
     while True:
         with sync_playwright() as p:
             browser = p.webkit.launch(
@@ -22,12 +27,15 @@ def main():
             page.goto(url, timeout=30000)
             print("Fetched site, waiting for element")
  
-            page.wait_for_selector(f"div[aria-label='{date}']", timeout=20000)
+            selector = f"div[aria-label='{date.replace('\'', '\\\'').replace('\"', '\\\"')}']"
+            page.wait_for_selector(selector, timeout=20000)
             print("Got element, waiting so it may load")
             time.sleep(1)
-            if "background-color: rgba(49, 200, 25, 0.2)" in page.query_selector(f"div[aria-label='{date}']").get_attribute("style"):
+            
+            element = page.query_selector(selector)
+            if element and "background-color: rgba(49, 200, 25, 0.2)" in element.get_attribute("style"):
                 ToastNotifier().show_toast(
-                    "Brighton ParkingReservation  Alert",
+                    "Parking Reservation Alert",
                     f"Parking spot for {date} might be available.",
                     duration=2,
                 )
